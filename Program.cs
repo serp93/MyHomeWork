ff282c95962c5b739396b2beb3ded7ea3c23ddbf
Sergei Repin <serega.93.ss@yandex.ru>
2022-04-15 20:59:04 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 1.
+/*
+Console.Write("Input first integer number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second integer number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1>num2)
+    Console.Write("maximum number " + num1);
+else
+    Console.Write("maximum number " + num2);
+    if (num1 == num2)
+        Console.Write(", the numbers are equal");
+*/
+// Задача 2.
+/*
+Console.Write("Input first integer number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second integer number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input third integer number: ");
+int c = Convert.ToInt32(Console.ReadLine());
+ 
+Console.WriteLine(Math.Max(a, Math.Max(b, c)));
+*/
+// Задача 3.
+/*
+Console.Write("Imput integer number: ");
+int а = Convert.ToInt32(Console.ReadLine());
+
+if (а % 2 == 0)
+    Console.Write("да");
+else
+    Console.Write("нет");
+*/
+// Задача 4.    
+/*
+Console.Write("Imput integer number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int current;
+
+if(N>0)
+{
+    current = 1;
+    while(current <= N)
+    {  
+        if(current % 2 == 0)
+        {
+            Console.Write(current + " ");
+            current++;
+        }
+        else
+            current++;
+    }
+}
+*/
