cbb7c3cc6006802e241c8bdf8c3577df70af4b18
Sergei Repin <serega.93.ss@yandex.ru>
2022-04-21 22:29:04 +0300

    Domashka

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 10.
+
+//  void SecondDigit()
+// {
+//     Console.Write("Enter a number from 100 to 999: ");
+//     int num = Convert.ToInt32(Console.ReadLine());
+//     int n2 = (num/10)%10;
+// if (num == 0 || num > 999 || num < 100)
+//     {
+//     Console.Write("incorrect input, try again");
+//     }
+// else
+//     {
+//     Console.Write(num + " -> ");
+//     Console.WriteLine(n2);
+//     }
+// }
+// SecondDigit();
+
+// Задача 13.
+
+//  void ThirdDigit()
+// {
+//     Console.Write("Enter a number from 100 to 999: ");
+//     int num = Convert.ToInt32(Console.ReadLine());
+//     int n3 = (num%100)%10;
+// if (num == 0 || num < 100)
+//     {
+//     Console.Write("третьей цифры нет");
+//     }
+// else
+//     {
+//     Console.Write(num + " -> ");
+//     Console.WriteLine(n3);
+//     }
+// }
+// ThirdDigit();
+
+// Задача 15.
+
+// void Weekend()
+// {
+// Console.Write("Enter the number of the day of the week: ");
+// int day = Convert.ToInt32(Console.ReadLine());
+
+// if (day > 7 || day < 1)
+//     {
+//     Console.WriteLine("Incorrect number of day!");
+//     }
+// else
+//     {
+//     if (day == 6 || day == 7)
+//         {
+//         Console.WriteLine("Weekend");
+//         }
+//     else
+//         {
+//         Console.WriteLine("Weekday");
+//         }
+//     }
+// }
+// Weekend();
