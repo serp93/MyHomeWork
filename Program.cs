668c0eea9df58084f07e2845c03777c425d374c2
Sergei <serega.93.ss@yandex.ru>
2022-05-26 22:10:59 +0300

    domashka9

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 66: 
+// Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+
+// Console.Write("Input M: ");
+// int M = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input N: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+// int SumNumbers(int M, int N)
+
+//     {
+//         return (N == M) ? M : N + SumNumbers(M, N - 1);
+//     }
+
+// Console.WriteLine(SumNumbers(M, N));
+
+
+
+// Задача 67: 
+// Напишите программу, 
+// которая будет принимать на вход число и возвращать сумму его цифр.
+
+
+// int SumNumber1(int n)
+
+// {
+//     return (n/10!=0) ? n%10 + SumNumber1 (n/10):n%10;
+// }
+// Console.Write("Input n: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(SumNumber1(n));
+
